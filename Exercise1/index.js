da450b614d7a65eaa73d7bf5dace27290751d65c
TeddPaul13 <teddtycoon@gmail.com>
2023-05-28 15:46:02 +1000

    Complete exercise 1 for module 5

---
+++ Exercise1/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+
+const app1 = express();
+const app2 = express();
+const app3 = express();
+
+const port1 = 3000;
+const port2 = 4000;
+const port3 = 5000;
+
+
+app1.get("/", (req, res) => {
+  res.send("Hello! Welcome to the first server");
+});
+
+app2.get("/", (req, res) => {
+    res.send("Hello! Welcome to the Second server");
+  });
+
+  app3.get("/", (req, res) => {
+    res.send("Hello! Welcome to the Third server");
+  });  
+
+
+app1.listen(port1, () => {
+  console.log(`Example app
+listening at
+http://localhost:${port1}`);
+});
+app2.listen(port2, () => {
+    console.log(`Example app
+  listening at
+  http://localhost:${port2}`);
+  });
+app3.listen(port3, () => {
+    console.log(`Example app
+  listening at
+  http://localhost:${port3}`);
+  });
